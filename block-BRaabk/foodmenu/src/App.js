d4666193042f0f1536998efa48b0bada92acd314
Vishalgoswami12 <vishalgoswami2250@gmail.com>
2022-09-07 23:23:02 +0530

    food app

---
+++ block-BRaabk/foodmenu/src/App.js
@@ -0,0 +1,13 @@
+import Allitem from "./component/Allitem";
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="center">Food Menu</h1>
+     <Allitem/>
+    </div>
+  );
+}
+
+export default App;
