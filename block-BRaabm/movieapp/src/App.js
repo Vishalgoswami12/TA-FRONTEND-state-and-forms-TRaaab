a0171820d8cc84a97f804457235fc15a9543736a
Vishalgoswami12 <vishalgoswami2250@gmail.com>
2022-09-08 15:48:56 +0530

    movie app

---
+++ block-BRaabm/movieapp/src/App.js
@@ -0,0 +1,14 @@
+import Aside from "./component/Aside";
+import Movie from "./component/Movie";
+
+
+function App() {
+  return (
+    <div className="App flex">
+     <Aside/>
+     <Movie/>
+    </div>
+  );
+}
+
+export default App;
